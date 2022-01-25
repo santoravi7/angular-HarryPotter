590ec165d9fe8ded72b536bcc24976f02ecaf283
santoravi7 <santoravi7@gmail.com>
2022-01-25 17:13:59 +0530

    1 file modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HarrypotterService } from './harrypotter.service';
 import { AppRoutingModule, routingComponents } from './app-routing/app-routing.module';
-import { CharactersComponent } from './characters/characters.component';
 import { NavComponent } from './nav/nav.component';
-import { GroupByPipe } from './group-by.pipe';
 import { DataStorage } from './data-storage';
-import { GroupbynamePipe } from './groupbyname.pipe';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { UserResolver } from './user.resolver';
 import { LoaderService } from './loader.service';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { LoaderInterceptor } from './loader.interceptor';
+
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule,Ng2SearchPipeModule,MatProgressSpinnerModule],
   declarations: [AppComponent,
     NavComponent,
     routingComponents,
-    GroupByPipe,
-    GroupbynamePipe
   ],
   bootstrap: [AppComponent],
   providers: [HarrypotterService,
     DataStorage, 
     UserResolver, 
-    LoaderService,
-    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
+    LoaderService  
   ],
 })
 export class AppModule {}
